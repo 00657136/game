0cd501e4a8fd0b990a1f22591549729944e470d9
claudio <jeff.797886@gmail.com>
2021-06-30 07:59:32 +0800

    Initial Commit

---
+++ game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  game
+//
+//  Created by 張凱翔 on 2021/6/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ game/gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameApp.swift
+//  game
+//
+//  Created by 張凱翔 on 2021/6/30.
+//
+
+import SwiftUI
+
+@main
+struct gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
